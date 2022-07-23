6b24d55c8e92e89bf2e64f8e4d6c4a3a8a524c58
bachtrananhduc <bachtrananhduc@gmail.com>
2022-07-23 09:06:42 +0700

    add js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// const style = require("../sass/abstracts/_variables");
+// // const style = require('sass-extract-loader!./_variables.scss');
+// const colorPrimary = style.global["$color-primary"];
+
+// console.log(colorPrimary);
